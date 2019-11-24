cdfc14d3fd42b0558e53a2f5a4083e252c7bda2d
Nils Plaschke <Chumper@users.noreply.github.com>
2019-11-25 00:50:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,22 +5,24 @@ drive=/dev/sda
 
 partition () {
   if /sbin/sfdisk -d ${drive} 2>&1 ; then
+    ### CONSTANTS
+    boot_size=512M
+    
     printf "Partitioning..."
     
-    available_memory=$(free -m | grep Mem | awk '{print $2}')
+    available_memory=$(free -b | grep Mem | awk '{print $2}')
     available_disk=$(fdisk -l | grep "${drive}" | awk '{print $5}')
     
-    boot_size=500000000 # 500M
-    swap_size=(( ${available_memory} / 2 ))
-    home_size=(( ${available_disk} - ${boot_size} - ${home_size} ))
+    swap_size="$(( ${available_memory} / 2 ))""
+    home_size="$(( ${available_disk} - $(numfmt --from=iec ${boot_size}) - ${hswap_size} ))""
 
     # partition the disk
-    echo -e "Total Memory:\t${available_memory}"
-    echo -e "Total Disk Size:\t${available_disk}"
+    echo -e "Total Memory:\t\t$(numfmt --to=iec ${available_memory})"
+    echo -e "Total Disk Size:\t$(numfmt --to=iec ${available_disk})"
     echo -e "--------"
-    echo -e "Boot Partition:\t$(numfmt --to=si ${boot_size})"
-    echo -e "Swap Partition:\t$(numfmt --to=si ${swap_size})"
-    echo -e "Home Partition:\t$(numfmt --to=si ${home_size})" 
+    echo -e "Boot Partition:\t\t$(numfmt --to=iec ${boot_size})"
+    echo -e "Swap Partition:\t\t$(numfmt --to=iec ${swap_size})"
+    echo -e "Home Partition:\t\t$(numfmt --to=iec ${home_size})" 
   fi
 }
 
