df539d3b7c66fd4a56b295cae96d659aedd9ef05
Nils Plaschke <Chumper@users.noreply.github.com>
2019-11-25 01:01:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@
 drive=/dev/sda
 
 partition () {
-  if /sbin/sfdisk -d ${drive} 2>&1 ; then
+  if ! /sbin/sfdisk -d ${drive} > /dev/null 2>&1 ; then
     ### CONSTANTS
     boot_size=512M
     
-    printf "Partitioning..."
+    echo "Partitioning..."
     
     available_memory=$(free -b | grep Mem | awk '{print $2}')
     available_disk=$(fdisk -l | grep "${drive}" | awk '{print $5}')
     
     swap_size="$(( ${available_memory} / 2 ))""
-    home_size="$(( ${available_disk} - $(numfmt --from=iec ${boot_size}) - ${hswap_size} ))""
+    home_size="$(( ${available_disk} - $(numfmt --from=iec ${boot_size}) - ${swap_size} ))""
 
     # partition the disk
     echo -e "Total Memory:\t\t$(numfmt --to=iec ${available_memory})"
     echo -e "Total Disk Size:\t$(numfmt --to=iec ${available_disk})"
     echo -e "--------"
-    echo -e "Boot Partition:\t\t$(numfmt --to=iec ${boot_size})"
+    echo -e "Boot Partition:\t\t${boot_size}"
     echo -e "Swap Partition:\t\t$(numfmt --to=iec ${swap_size})"
     echo -e "Home Partition:\t\t$(numfmt --to=iec ${home_size})" 
   fi
