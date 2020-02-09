6addacc7bfcf33215aafe0e0c1789834c9f351da
Nils Plaschke <Chumper@users.noreply.github.com>
2020-02-09 21:23:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -366,9 +366,9 @@ download_configs () {
 
 early_kms () {
     if ! command -v arch-chroot > /dev/null 2>&1; then
-        if ! grep -q "MODULES=()" /etc/mkinitcpio.conf; then
+        if grep -q "MODULES=()" /etc/mkinitcpio.conf; then
             echo -e "\n######## Initiate early KMS...\n"
-            sed -i .bak 's/MODULES=()/MODULES(amdgpu)/' /etc/mkinitcpio.conf
+            sed -i .bak 's/MODULES=()/MODULES=(amdgpu)/' /etc/mkinitcpio.conf
             mkinitcpio -P
         fi
     fi
